a48658eac5dab81c1f9834551a96733400709b85
HugoLattard <hugolattard@gmail.com>
2020-12-08 23:32:08 +0100

    sidebar OK

--- src/components/OffersSmall.js
+++ src/components/OffersSmall.js
@@ -1,20 +1,22 @@
 import React, {useContext} from 'react'
 import "../main.css"
 import styled from "styled-components"
+import {ToggleContext} from "../context/toggleContext"
 
 const OffersSmall = () => {
+         const {toggleSearch} = useContext(ToggleContext)
         return (
                 <Wrapper>
-                        <div className="button-container">
-                               <button>
+                        <div className="button-container section">
+                               <button className="btn" >
                                        Toute la France
                                </button>
-                               <button
-                        //        onClick={
-                        //                toggletoggleSearch
-                        //        }
+                               <button  className="btn"
+                               onClick={
+                                       toggleSearch
+                               }
                                >Filtres</button>
-                               <button>Tri: Plus récentes</button>
+                               <button className="btn" >Tri: Plus récentes</button>
                         </div>
                         <div className="check-box-container"></div>
                         <div className="offers-container"></div>
@@ -30,5 +32,18 @@ min-height: 100vh;
 z-index: 0; 
 grid-template-rows: 3rem 4rem auto; 
 grid-row-gap: 1rem;
-background: black;
+/* background: black; */
+.button-container {
+grid-row: 1/2;
+
+        button {
+        font-size: 0.85rem;
+        border: 1px solid var(--clr-grey-2);
+        border-radius: 25px;
+        outline: none; 
+        margin-left: 0; 
+        margin: 0.5rem; 
+}
+}
+
 `
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "../main.css"
 import styled from "styled-components"
-import { BiBell, BiHeart, BiMessageDetail, BiUser, BsPlusSquare, GiMagnifyingGlass, GoThreeBars, ImCross } from 'react-icons/all'
+import { BiBell,  BiHeart, BiMessageDetail, BsPlusSquare, FaChevronRight, GiMagnifyingGlass, ImCross } from 'react-icons/all'
 import logo from "../icons/logo.png"
 
 const SideBar = ({sideBarOpen, toggleSide}) => {
@@ -19,6 +19,71 @@ onClick={toggleSide}
         />
 </button>
                                 </header>
+                                <div className="top section">
+<div className="s-b-left">
+        <BsPlusSquare 
+        fontSize="1.5rem"
+        style={{color : "black", marginRight: "1rem"}}
+        /> <span>Déposer une annonce</span>
+</div>
+<div className="s-b-left ">
+        <GiMagnifyingGlass 
+        fontSize="1.5rem"
+        style={{color : "black", marginRight: "1rem"}}
+        />
+        <span>Rechercher</span>
+</div>
+<div className="b-bottom"></div>
+<div className="s-b-left">
+        <BiMessageDetail 
+         fontSize="1.5rem"
+        style={{color : "black", marginRight: "1rem"}}
+        />
+         <span>Messages</span>
+</div>
+<div className="s-b-left">
+        <BiHeart 
+         fontSize="1.5rem"
+        style={{color : "black", marginRight: "1rem"}}
+        />
+         <span>Favoris</span>
+</div>
+<div className="s-b-left">
+        <BiBell 
+        fontSize="1.5rem"
+        style={{color : "black", marginRight: "1rem"}}
+        />
+        <span>Recherches sauvegardées</span>
+</div>
+                                </div>
+                                <div className="bottom section">
+<div className="s-b-left">
+        <span
+        style={{color: "#FF6E13"}}
+        >Se connecter</span>
+</div>
+<div className="b-bottom"></div>
+<div className="s-b">
+
+        <span>Informations pratiques</span>
+        <FaChevronRight
+         fontSize="1.5rem"
+        style={{color : "#939ea9", marginRight: "6rem"}}
+        />
+</div>
+<div className="s-b">
+        <span>leboncoin Groupe</span>
+         <FaChevronRight 
+         fontSize="1.5rem"
+        style={{color : "#939ea9", marginRight: "6rem"}}
+        />
+</div>
+
+                                </div>
+
+                                        
+                                
+
                         </section>
                 </Wrapper>
         )
@@ -27,7 +92,10 @@ onClick={toggleSide}
 export default SideBar
 
 const Wrapper = styled.aside`
-  
+ .show-sidebar {
+         opacity: 1;
+         transform: translateX(0)
+} 
   section {
   position: fixed;
   top: 0;
@@ -39,12 +107,13 @@ z-index: 100;
  opacity: 0;
    transition: var(--transition);
   transform: translateX(-100%);
-  display: flex;
-  flex-direction: column; 
+  display: grid;
+  grid-template-rows: 4rem 50vh 1fr; 
+  row-gap: 0.5rem;
 
   }
 header {
-        height: 4rem;
+        grid-row: 1/2;
         position: relative;
         display: flex; 
         justify-content: center; 
@@ -58,12 +127,35 @@ header {
                 right: 1rem;
         }
 }
+.top {
+        grid-row: 2/3;
+        div {
+                margin : 0.5rem auto;
+                padding: 0.5rem 0; 
+        }
+        span {
+font-size: 1rem;
 
-
-.show-sidebar {
-         opacity: 1;
-         transform: translateX(0)
+        }
+        
 }
+.b-bottom {
+        border-bottom: 1px solid var(--clr-grey-2);
+        
+}
+.bottom {
+        grid-row: 3/4;
+          div {
+                margin : 0.5rem auto; 
+                 padding: 0.5rem 0; 
+        }
+        span {
+font-size: 1rem;
+        }
+}
+
+
+
   
 
 `
